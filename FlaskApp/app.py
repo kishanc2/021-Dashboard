a81e602d81cda8208feafea816522ca6cf8666be
Kishan Challagulla <67973935+kishanc2@users.noreply.github.com>
2023-08-17 22:44:36 -0500

    finished-1

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -39,6 +39,26 @@ def president():
 def vice_president():
     return render_template('vice_president.html')
 
+@app.route('/vice_president2.html', methods = ['GET', 'POST'])
+def vice_president2():
+    return render_template('vice_president2.html')
+
+@app.route('/community_director.html', methods = ['GET', 'POST'])
+def community_director():
+    return render_template('community_director.html')
+
+@app.route('/outreach_director.html', methods = ['GET', 'POST'])
+def outreach_director():
+    return render_template('outreach_director.html')
+
+@app.route('/marketing_director.html', methods = ['GET', 'POST'])
+def marketing_director():
+    return render_template('marketing_director.html')
+
+@app.route('/contact.html', methods = ['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
 
